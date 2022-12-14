9c5acc2f8733ec88d5a786b79d70a74e2a718b69
HalaAshraf <81113286+HalaAshraf@users.noreply.github.com>
2022-12-14 23:35:42 +0200

    xml node

---
+++ Hala's Progress/Node.java
@@ -0,0 +1,43 @@
+package xmlproj;
+import java.util.*;
+public class Node {
+
+	private String tag;
+    private String data;
+    private List<Node> children = new ArrayList<>();
+    
+    public Node(String t, String d)
+    {
+        this.tag = t;
+        this.data =d;
+    }
+ 
+    public void addChild(Node n)
+    {
+    
+        this.children.add(n);
+    }
+    
+    public String getTag()
+    {
+        return this.tag;
+    }
+    
+    public String getData()
+    {
+        return this.data;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+ 
+}
