b170cdb229af3124fbb78deef3a8f4434a9e53f5
Ismail Salama Abolsoud Saleh <60485490+ismail939@users.noreply.github.com>
2022-12-16 20:33:12 +0200

    Detect and show error function
    it successfully passed the three errors in the presentation file.

--- Ismail's Progress/Main.java
+++ Ismail's Progress/Main.java
@@ -1,23 +1,108 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String []args)throws Exception{
-            Pattern pattern= Pattern.compile("<(\\w+)>");
-            Matcher matcher;
-            FileReader fr=new FileReader("H:\\note.xml");
-            BufferedReader br=new BufferedReader(fr);
-            String line=br.readLine();
-            while(line!=null){
-                matcher=pattern.matcher(line);
-                if(matcher.find())
-                    System.out.println(matcher.group());
-                line=br.readLine();
-            }
-
-
-           br.close();
+    class ET {
+        int lineNumber;
+        String content;
+        ET(int l, String s){
+            this.lineNumber = l;
+            this.content = s;
         }
     }
 
+    public static void printStack(Stack<ET> st) {
+        while (!st.isEmpty()){
+            System.out.println(st.peek().content);
+            st.pop();
+        }
+        System.out.println(".............");
+    }
+    public static void solveError(){
+
+    }
+
+    public static boolean consistency(BufferedReader br) throws IOException {
+        Pattern opening_tag= Pattern.compile("<(\\w+)>"), closing_tag= Pattern.compile("</(\\w+)>");
+        Matcher matcher_opening_tag, matcher_closing_tag;
+        Stack<String> stack = new Stack<String>();
+        String line=br.readLine();
+        while(line!=null){
+            matcher_opening_tag=opening_tag.matcher(line);
+            matcher_closing_tag=closing_tag.matcher(line);
+            if(matcher_opening_tag.find()){
+                stack.push(matcher_opening_tag.group(1));
+            }
+            if(matcher_closing_tag.find()) {
+                if ((stack.peek().equals(matcher_closing_tag.group(1)))) {
+                    stack.pop();
+                }
+                else return false;
+            }
+            line=br.readLine();
+        }
+        return true;
+    }
+
+    public void showError(BufferedReader br) throws IOException {
+        int lineNumber=1;
+        Pattern opening_tag= Pattern.compile("<(\\w+)>"), closing_tag= Pattern.compile("</(\\w+)>");
+        Matcher matcher_opening_tag, matcher_closing_tag;
+        Stack<ET> stack = new Stack<ET>();
+
+        String line=br.readLine();
+        while(line!=null){
+
+            matcher_opening_tag=opening_tag.matcher(line);
+            matcher_closing_tag=closing_tag.matcher(line);
+            boolean open=matcher_opening_tag.find(),close=matcher_closing_tag.find();
+            if(open){
+                ET error = new ET(lineNumber,matcher_opening_tag.group(1));
+                stack.push(error);
+            }
+            if(open&&close){
+                if(matcher_opening_tag.group(1).equals(matcher_closing_tag.group(1))){
+                    stack.pop();
+                }
+                else{ // like <name>2</id>
+                    System.out.println("error in line: " +stack.peek().lineNumber);
+                    System.out.println("Missing closing tag </"+stack.peek().content+">" );
+                    stack.pop();
+                    System.out.println("error in line: "+lineNumber);
+                    System.out.println("There is an additional closing tag: </"+matcher_closing_tag.group(1)+">");
+                }
+            }
+            else if(close) {
+                while(!(stack.peek().content.equals(matcher_closing_tag.group(1)))) {
+                    System.out.println("error in line: " +stack.peek().lineNumber);
+                    System.out.println("Missing closing tag </"+stack.peek().content+">" );
+                    stack.pop();
+
+                }
+
+                stack.pop();
+
+
+            }
+            line=br.readLine();
+            lineNumber++;
+        }
+        if(!stack.isEmpty()){
+            System.out.println("error in line: " + stack.peek().lineNumber);
+            System.out.println("Missing closing tag </"+stack.peek().content+">" );
+        }
+    }
+
+    public static void main(String []args)throws Exception{
+        FileReader fr=new FileReader("H:\\note.xml");
+        BufferedReader br=new BufferedReader(fr);
+        Main x = new Main();
+        //if(consistency(br)) System.out.println("100%");
+        //else System.out.println("eldonia msh tmam");
+        x.showError(br);
+    }
+}
