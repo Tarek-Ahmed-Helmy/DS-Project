dad2c27f6f008257040fdd6a68626423f585c63f
ismail939 <ismailsalama526@gmail.com>
2023-01-28 14:07:06 +0200

    GraphNode and some other file that will be edited

---
+++ Project/GraphNode.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;  // this is a undirected graph and we neeed to have a directed graph i think
+
+class GraphNode {
+	// data members of the graphNode
+	 public String id;
+	 public String name;
+	 public ArrayList<String> posts=new ArrayList<String>();
+	 public ArrayList<String> followersIds=new ArrayList<String>();
+	
+	 
+
+}
+
+
