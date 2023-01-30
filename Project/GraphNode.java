3f7518826139d92767100b35269af9a2b26a4c0a
ismail939 <ismailsalama526@gmail.com>
2023-01-31 00:38:42 +0200

    update

--- Project/GraphNode.java
+++ Project/GraphNode.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;  // this is an undirected graph, and we need to have a directed graph I think
+import java.util.ArrayList;  
 
 class GraphNode {
 	// data members of the graphNode
-	public String id;
+	public String id; 
 	public String name;
 	public ArrayList<String> posts= new ArrayList<>();
 	public ArrayList<String> topics= new ArrayList<>();
