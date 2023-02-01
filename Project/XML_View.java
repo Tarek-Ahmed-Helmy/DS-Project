c359c8e2cff600a389c65c04d2277dddb2757a82
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2023-02-02 00:22:27 +0200

    update

--- Project/XML_View.java
+++ Project/XML_View.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
