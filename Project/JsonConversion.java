d951e47aceed179ebd180cb51fb6b92dcc6d0c14
HalaAshraf <81113286+HalaAshraf@users.noreply.github.com>
2023-02-01 12:19:29 +0200

    Update JsonConversion.java

--- Project/JsonConversion.java
+++ Project/JsonConversion.java
@@ -1,29 +1,35 @@
 import java.util.Objects;
 
-public class JsonConversion {
-	public static void printJSON(Node node){
-		System.out.print("\t"+"\"" + node.getTag() + "\"" + ":");
-
-		if (Objects.equals(node.getData(), "")){
+public static void printJSON(Node node) 
+	{	
+		System.out.print("\"" + node.getTag() + "\"" + ":");
+		
+		if (node.getData() == "")
+		{
 			System.out.print("[\n\t");
-			System.out.print("\t\t{\n\t");
+			System.out.print("{\n\t");
+	
 		}
-		else{
-			System.out.print("\"" + node.getData() + "\"");
+		else{	
+			System.out.print("\"" + node.getData() + "\"");	
+
 		}
 
-		for (int i = 0; i < node.getChildren().size(); i++){
-			if (node.getChildren().get(i) != null && i<node.getChildren().size()-1){
-				System.out.print(" \t");
-				printJSON(node.getChildren().get(i));
+		for (int i = 0; i < node.getChildren().size(); i++)
+		{
+
+			if (node.getChildren().get(i) != null && i<node.getChildren().size()-1)
+			{
+			//	System.out.print("\t");
+				printJSON(node.getChildren().get(i));	
 				System.out.print(",\n\t");
 			}
-			if( i==node.getChildren().size()-1){
+			if( i==node.getChildren().size()-1)
+			{
 				printJSON(node.getChildren().get(i));
 				System.out.println("\n\t\t}");
 				System.out.print("\t]");
+			
 			}
 		}
-		System.out.println();
-	}
 }
